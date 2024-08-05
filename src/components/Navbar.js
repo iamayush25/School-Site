632c882dba598c5d13fdc9b38324dd52cecd2b66
Ayush Sharma <ayush22@navgurukul.org>
2024-08-05 18:39:59 +0530

    Responsive Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
+import './Navbar.css';
 
 const NavbarItems = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link as={Link} to="/">
-            Home
-          </Nav.Link>
-          <Nav.Link as={Link} to="/about">
-            About Us
-          </Nav.Link>
-          <Nav.Link as={Link} to="/academics">
-            Academics
-          </Nav.Link>
-          <Nav.Link as={Link} to="/admissions">
-            Admissions
-          </Nav.Link>
-          <Nav.Link as={Link} to="/faculty">
-            Faculty
-          </Nav.Link>
-          <Nav.Link as={Link} to="/students">
-            Students
-          </Nav.Link>
-          <Nav.Link as={Link} to="/gallery">
-            Gallery
-          </Nav.Link>
-          <Nav.Link as={Link} to="/contact">
-            Contact Us
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+    <Navbar bg="dark" variant="dark" className="navbarMain">
+      <Nav className="navbarItems">
+        <Nav.Link as={Link} to="/" className="mx-2">
+          Home
+        </Nav.Link>
+        <Nav.Link as={Link} to="/about" className="mx-2">
+          About Us
+        </Nav.Link>
+        <Nav.Link as={Link} to="/academics" className="mx-2">
+          Academics
+        </Nav.Link>
+        <Nav.Link as={Link} to="/admissions" className="mx-2">
+          Admissions
+        </Nav.Link>
+        <Nav.Link as={Link} to="/faculty" className="mx-2">
+          Faculty
+        </Nav.Link>
+        <Nav.Link as={Link} to="/students" className="mx-2">
+          Students
+        </Nav.Link>
+        <Nav.Link as={Link} to="/gallery" className="mx-2">
+          Gallery
+        </Nav.Link>
+        <Nav.Link as={Link} to="/contact" className="mx-2">
+          Contact Us
+        </Nav.Link>
+      </Nav>
     </Navbar>
   );
 };
